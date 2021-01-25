52fe9e38a2c4c29513eb9a41f800c8e9ce4f430c
Diego Petrucci <baulei@gmail.com>
2021-01-25 19:15:23 +0000

    Only switch between XS or XL

--- src/labeler.sh
+++ src/labeler.sh
@@ -36,9 +36,7 @@ labeler::label_for() {
 
   if [ "$total_modifications" -lt "$xs_max_size" ]; then
     label="Trivial"
-  elif [ "$total_modifications" -lt "$m_max_size" ]; then
-#     label="size/m"
-  else
+  elif if [ "$total_modifications" \> "$m_max_size" ]; then
     label="size/xl"
   fi
 
